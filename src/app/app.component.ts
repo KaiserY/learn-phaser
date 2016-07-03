bd918c8614ca8c4f166aa48670dbf001f40581d9
yang yue <yang yue>
2016-07-04 00:13:14 +0800

    wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ViewEncapsulation } from "@angular/core";
+import { MdButton } from '@angular2-material/button';
 import { MdToolbar } from '@angular2-material/toolbar';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
 
 import "../../node_modules/material-design-icons/iconfont/material-icons.css";
 import "../public/css/material-icons.css";
@@ -10,6 +12,11 @@ import "../public/css/styles.css";
   selector: "ng-app",
   styles: [require("./app.component.css")],
   template: require("./app.component.html"),
-  directives: [MdToolbar]
+  encapsulation: ViewEncapsulation.None,
+  directives: [
+    MdToolbar,
+    MD_SIDENAV_DIRECTIVES,
+    MdButton
+  ]
 })
 export class AppComponent { }
