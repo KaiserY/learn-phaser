1bc5afa79c3855dcd1182ec41aa2e05d24aaf8d8
yang yue <yang yue>
2016-07-08 00:25:32 +0800

    wip

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MdButton } from '@angular2-material/button';
 
 @Component({
   selector: "ng-files",
@@ -8,7 +9,27 @@ import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
   template: require("./files.component.html"),
   directives: [
     MD_SIDENAV_DIRECTIVES,
-    MD_LIST_DIRECTIVES
+    MD_LIST_DIRECTIVES,
+    MdButton
   ]
 })
-export class FilesComponent { }
+export class FilesComponent {
+  expandmore: boolean = false;
+  expandless: boolean = false;
+
+  toggle() {
+    if (!this.expandmore && !this.expandless) {
+      this.expandmore = true;
+      this.expandless = false;
+    } else if (this.expandmore) {
+      this.expandmore = false;
+      this.expandless = true;
+    } else if (this.expandless) {
+      this.expandmore = true;
+      this.expandless = false;
+    } else {
+      this.expandmore = false;
+      this.expandless = false;
+    }
+  };
+}
